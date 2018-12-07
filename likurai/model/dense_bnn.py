e5ad7f7fc01ef01d8e38b8699f28c0aa23dab84d
Ben Glickenhaus <benglickenhaus@gmail.com>
2018-12-07 14:40:30 -0500

    Fixed a small bug
    Next TODO would be convolutional layers I guess

--- likurai/model/dense_bnn.py
+++ likurai/model/dense_bnn.py
@@ -34,7 +34,13 @@ class BayesianNeuralNetwork(Model):
 
     def predict(self, x, n_samples=1, progressbar=True, point_estimate=False):
         self.x.set_value(x)
-        self.y.set_value(np.zeros((np.array(x).shape[0], self.y.get_value().shape[1])).astype(floatX))
+        try:
+            # For classification tasks
+            self.y.set_value(np.zeros((np.array(x).shape[0], self.y.get_value().shape[1])).astype(floatX))
+        except IndexError:
+            # For regression tasks
+            self.y.set_value(np.zeros(np.array(x).shape[0]).astype(floatX))
+
         with self.model:
             ppc = pm.sample_ppc(self.trace, samples=n_samples, progressbar=progressbar)
         if point_estimate:
