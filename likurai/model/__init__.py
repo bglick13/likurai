a5dc8bbf3769817cc9ac696b9cc6577e02f0980d
Ben Glickenhaus <benglickenhaus@gmail.com>
2019-02-01 09:48:04 -0500

    i mean it runs
    I want to add some sort of monitoring/measuring in so i can tell if it's learning anything useful

--- likurai/model/__init__.py
+++ likurai/model/__init__.py
@@ -1,8 +1,8 @@
 from .model import Model
 from .bnn import BayesianNeuralNetwork, HierarchicalBayesianNeuralNetwork, TFPNetwork
-from .gan import ConditionalRCGAN, ConditionalSeqGAN
+from .gan import ConditionalSeqGAN, Generator, Discriminator
 
-__all__ = ['BayesianNeuralNetwork', 'HierarchicalBayesianNeuralNetwork', 'TFPNetwork', 'ConditionalRCGAN',
-           'ConditionalSeqGAN']
+__all__ = ['BayesianNeuralNetwork', 'HierarchicalBayesianNeuralNetwork', 'TFPNetwork',
+           'ConditionalSeqGAN', 'Generator', 'Discriminator']
 
 
--- likurai/model/gan.py
+++ likurai/model/gan.py
@@ -1,48 +1,168 @@
 """
 This file will contain objects to create various forms of GANs
 """
+import torch
+import torch.autograd as autograd
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import torch.optim as optim
 import numpy as np
-from keras.models import Model, load_model, save_model
-from keras.layers import LSTM, Input, Concatenate, Dense, TimeDistributed, RepeatVector, Embedding, Multiply, Flatten, BatchNormalization
-import keras.backend as K
-from keras.losses import binary_crossentropy
-from keras.optimizers import Adam, RMSprop
+import math
+import torch.nn.init as init
+
+
+import numpy as np
 import os
-import tensorflow as tf
-import tensorflow_probability as tfp
-floatX = tf.float32
 
 
-class GAN:
+class Discriminator(nn.Module):
+    def __init__(self, n_classes, conditional_data_size, embedding_dim, hidden_size, sequence_length, n_hidden=2,
+               dropout=0.2):
+        super(Discriminator , self).__init__()
+        self.n_classes = n_classes
+        self.conditional_data_sie = conditional_data_size
+        self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.sequence_length = sequence_length
+        self.n_hidden = n_hidden
+        self.dropout = dropout
 
-    def __init__(self):
-        self.discriminator = None
-        self.generator = None
-        self.combined = None
-        self.build_models()
+        self.sequence_embedding = nn.Embedding(self.n_classes, self.embedding_dim)
+        self.conditional_embeddding = nn.Embedding(self.conditional_data_sie, self.embedding_dim)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_size, num_layers=self.n_hidden, bidirectional=True, dropout=self.dropout)
+        self.hidden = nn.Linear(self.hidden_size*2*self.n_hidden, self.hidden_size)
+        self.dropout_hidden_to_out = nn.Dropout(p=self.dropout)
+        self.out = nn.Linear(self.hidden_size, 1)
 
-    def load_model(self, filepath):
-        self.discriminator = load_model(os.path.join(filepath, 'discriminator.h5'))
-        self.generator = load_model(os.path.join(filepath, 'generator.h5'))
-        self.combined = load_model(os.path.join(filepath, 'combined.h5'))
+    def forward(self, sequence_input, conditional_input, hidden):
+        conditional_input = torch.LongTensor(conditional_input).cuda().unsqueeze(1)
 
-    def save_model(self, filepath):
-        self.discriminator.save(os.path.join(filepath, 'discriminator.h5'))
-        self.generator.save(os.path.join(filepath, 'generator.h5'))
-        self.combined.save(os.path.join(filepath, 'combined.h5'))
+        # sequence_input = sequence_input.permute(1, 0, 2)
 
-    def build_discriminator(self):
-        return Model()
+        sequence_emb = self.sequence_embedding(sequence_input)
+        conditional_emb = self.conditional_embeddding(conditional_input)
+        emb = torch.mul(sequence_emb, conditional_emb)
+        # emb = emb.view(1, -1, self.embedding_dim)
+        emb = emb.permute(1, 0, 2)
+        _, hidden = self.gru(emb, hidden)
+        # hidden = hidden.permute(1, 0, 2).contiguous()
+        out = self.hidden(hidden.view(-1, self.n_hidden*2*self.hidden_size))
+        out = torch.tanh(out)
+        out = self.dropout_hidden_to_out(out)
+        out = self.out(out)
+        out = torch.sigmoid(out)
+        return out
 
-    def build_generator(self):
-        return Model()
+    def init_hidden(self, batch_size=1):
+        h = autograd.Variable(torch.zeros(self.n_hidden*2, batch_size, self.hidden_size))
+        return h.cuda()
 
-    def build_models(self):
-        pass
+    def predict_on_batch(self, sequence_input, conditional_input, batch_size=1):
+        h = self.init_hidden(batch_size)
+        out = self.forward(sequence_input, conditional_input, h)
+        return out.view(-1)
 
-# class Generator:
-#
-#     def __init__(self):
+    def loss(self, pred, true):
+        loss_fn = nn.BCELoss()
+        return loss_fn(pred, true)
+
+
+class Generator(nn.Module):
+    def __init__(self, n_classes, conditional_data_size, embedding_dim, hidden_size, sequence_length):
+        super(Generator, self).__init__()
+        self.n_classes = n_classes
+        self.conditional_data_size = conditional_data_size
+        self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.sequence_length = sequence_length
+
+        self.sequence_embedding = nn.Embedding(self.n_classes, self.embedding_dim)
+        self.conditional_embeddding = nn.Embedding(self.conditional_data_size, self.embedding_dim)
+        self.gru = nn.GRU(self.embedding_dim, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.n_classes)
+
+    def init_hidden(self, batch_size=1):
+        h = autograd.Variable(torch.zeros(1, batch_size, self.hidden_size))
+        return h.cuda()
+
+    def forward(self, sequence_input, conditional_input, hidden):
+        # sequence_input = torch.LongTensor(sequence_input)
+        # conditional_input = torch.LongTensor(conditional_input)
+        #
+        # sequence_input = sequence_input.cuda()
+        # conditional_input = conditional_input.cuda()
+
+        sequence_emb = self.sequence_embedding(sequence_input)
+        conditional_emb = self.conditional_embeddding(conditional_input)
+
+        emb = torch.mul(sequence_emb, conditional_emb)
+        emb = emb.view(1, -1, self.embedding_dim)
+        emb = emb.permute(1, 0, 2)
+        out, hidden = self.gru(emb, hidden)  # input (seq_len, batch_size, n_features) --> (seq_len, batch_size, hidden)
+        out = out.view(-1, self.hidden_size)
+        out = self.out(out)
+        out = F.log_softmax(out, dim=1)
+        return out, hidden
+
+    # TODO: This
+    def sample(self, conditional_input, batch_size, n_samples=1):
+        """
+        Creates n_samples full sequences from scratch - basically the predict function
+        :param conditional_input: (batch_size, conditional_data_size)
+        :return: (batch_size, 18, 1)
+        """
+        samples = torch.zeros(batch_size, self.sequence_length).type(torch.LongTensor).cuda()
+        conditional_input = torch.LongTensor(conditional_input).cuda().unsqueeze(1)
+        h = self.init_hidden(1)
+        sequence_input = autograd.Variable(torch.Tensor([0]*batch_size)).type(torch.LongTensor).cuda().unsqueeze(1)
+        for i in range(self.sequence_length):
+            out, h = self.forward(sequence_input, conditional_input, h)
+            out = torch.multinomial(torch.exp(out), 1)
+            samples[:, i] = out.view(-1).data
+            sequence_input = out.view(-1).unsqueeze(1)
+
+        return samples
+
+    def do_rollout(self, sequence_input, conditional_input, from_scratch):
+        # Make sure the inputs are PyTorch Tensors
+        if isinstance(sequence_input, (list, np.ndarray)):
+            rollout = torch.LongTensor(sequence_input)
+            rollout = rollout.cuda()
+            sequence_input = torch.LongTensor(sequence_input).cuda()
+        else:
+            rollout = sequence_input
+
+        # h = self.init_hidden(sequence_input.size()[0])
+        h = self.init_hidden(1)
+        # Make inputs batch size of 1
+        conditional_input = torch.LongTensor(conditional_input).cuda().unsqueeze(0)
+        sequence_input = sequence_input.unsqueeze(0)
+        # rollout = torch.cat((rollout, torch.zeros(1, self.max_seq_len - len(sequence_input))))
+
+        if from_scratch:
+            n_samples = self.sequence_length + 1
+            logits = torch.zeros(n_samples, self.n_classes)
+        else:
+            n_samples = self.sequence_length
+
+        i = 0
+        while rollout.size()[0] < n_samples:  # Adding 1 for the start token
+            out, h = self.forward(sequence_input, conditional_input, h)
+            if from_scratch:
+                logits[i, :] = out.view(-1)
+                i += 1
+            out = torch.multinomial(torch.exp(out), 1)
+            rollout = torch.cat((rollout, out.view(-1)))
+            sequence_input = out.view(-1)
+
+        if from_scratch:
+            return rollout[1:].unsqueeze(0), logits[:-1].unsqueeze(0)
+        return rollout.unsqueeze(0)
+
+    def pg_loss(self, pred, rewards):
+        # TODO: I'm not convinced this is right but I want to get everything working first
+        return torch.sum(torch.mul(pred, rewards).mul(-1))
 
 
 class ConditionalSeqGAN:
@@ -219,112 +339,3 @@ class ConditionalSeqGAN:
 
             d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
             print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
-
-
-# TODO: Implement MCTS (i.e., the prediction at a certain timestep should be dependent on its previous predictions)
-# TODO: Implement SeqGAN (i.e., GAN that works on sequences of discrete data)
-# TODO: Implement experience replay for generators (i.e., train on its previously generated samples)
-class ConditionalRCGAN(GAN):
-
-    def __init__(self, n_discriminator_layers, discriminator_hidden_size, n_generator_layers, generator_hidden_size,
-                 generator_embed_size, generator_latent_dim, conditional_data_size, n_classes, sequence_length, learning_rate):
-        self.n_discriminator_layers = n_discriminator_layers
-        self.n_generator_layers = n_generator_layers
-        self.conditional_data_size = conditional_data_size
-        self.generator_hidden_size = generator_hidden_size
-        self.generator_latent_dim = generator_latent_dim
-        self.generator_embed_size = generator_embed_size
-        self.discriminator_hidden_size = discriminator_hidden_size
-        self.n_classes = n_classes
-        self.sequence_length = sequence_length
-        self.learning_rate = learning_rate
-        super().__init__()
-
-    # TODO: Should these really be time distributed or a) an LSTM(1) b) a single Dense layer (for Disc anyway)
-    # TODO: Is it feasible to implement a rollout for the generator here?
-
-    def build_discriminator(self):
-        inputs = Input(shape=(self.sequence_length, 1))
-        # c = Input(shape=(self.conditional_data_size, ))
-        c = Input(shape=(1, ))
-        embedding = Embedding(self.conditional_data_size, self.generator_latent_dim)(c)
-        embedding = Flatten()(embedding)
-        c_stacked = RepeatVector(self.sequence_length)(embedding)
-        h = Multiply()([inputs, c_stacked])
-        for _ in range(self.n_discriminator_layers):
-            h = LSTM(self.discriminator_hidden_size, return_sequences=True, activation='tanh')(h)
-            # h = BatchNormalization()(h)
-        output = TimeDistributed(Dense(1, activation='sigmoid'))(h)
-        model = Model(inputs=[inputs, c], outputs=output)
-        print("Discriminator summary...")
-        model.summary()
-        return model
-
-    def build_generator(self):
-        z = Input(shape=(self.sequence_length, self.generator_latent_dim))
-        # c = Input(shape=(self.conditional_data_size, ))
-        c = Input(shape=(1,))
-        embedding = Embedding(self.conditional_data_size, self.generator_latent_dim, input_length=1)(c)
-        embedding = Flatten()(embedding)
-        c_stacked = RepeatVector(self.sequence_length)(embedding)
-        h = Multiply()([z, c_stacked])
-        for _ in range(self.n_generator_layers):
-            h = LSTM(self.generator_hidden_size, return_sequences=True, activation='tanh')(h)
-            # h = BatchNormalization()(h)
-
-        outputs = TimeDistributed(Dense(1, activation='tanh'))(h)
-        model = Model(inputs=[z, c], outputs=outputs)
-        print("Generator summary...")
-        model.summary()
-        return model
-
-    def build_models(self):
-        self.discriminator = self.build_discriminator()
-        self.generator = self.build_generator()
-
-        self.discriminator.compile(RMSprop(self.learning_rate), loss='binary_crossentropy', metrics=['accuracy'])
-        self.generator.compile(RMSprop(self.learning_rate), loss='mean_squared_error')
-
-        z = Input(shape=(self.sequence_length, self.generator_latent_dim))
-        c = Input(shape=(1, ))
-        generated = self.generator([z, c])
-
-        self.discriminator.trainable = False
-        valid = self.discriminator([generated, c])
-
-        self.combined = Model([z, c], valid)
-        self.combined.compile(loss='binary_crossentropy', optimizer=RMSprop(self.learning_rate))
-
-    def bootstrap_generator(self, x, y, epochs, batch_size=128):
-        # TODO: This should train the generator to predict the next token (i.e., standard RNN training)
-        for epoch in range(epochs * (len(x) // batch_size)):
-            idx = np.random.randint(0, len(x), batch_size)
-            real_data, c = y[idx], x[idx]
-            z = np.random.normal(0, 1, (batch_size, self.sequence_length, self.generator_latent_dim))
-            loss = self.generator.train_on_batch([z, c], real_data)
-            print("Generator bootstrap loss: {}".format(loss))
-
-    def train(self, x, condition, epochs, batch_size=128, sample_interval=100, training_ratio=1):
-        valid = np.ones((batch_size, self.sequence_length, 1))
-        fake = np.zeros((batch_size, self.sequence_length, 1))
-
-        for epoch in range(epochs * (len(x) // batch_size)):
-            # Train Discriminator
-            for d_run in range(training_ratio):
-                idx = np.random.randint(0, len(x), batch_size)
-                real_data, c = x[idx], condition[idx]
-                z = np.random.normal(0, 1, (batch_size, self.sequence_length, self.generator_latent_dim))
-                generated = self.generator.predict([z, c])
-
-                d_loss_real = self.discriminator.train_on_batch([real_data, c], valid)
-                d_loss_fake = self.discriminator.train_on_batch([generated, c], fake)
-                d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
-
-            g_loss = self.combined.train_on_batch([z, c], valid)
-
-            # Plot the progress
-            print("%d [D loss: %f, acc.: %.2f%%] [G loss: %f]" % (epoch, d_loss[0], 100 * d_loss[1], g_loss))
-
-    def sample(self, condition):
-        z = np.random.normal(0, 1, (len(condition), self.sequence_length, self.generator_latent_dim))
-        return self.generator.predict([z, condition])
